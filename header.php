662ad41d4c57d72455735cbce169731470cea10d
tutorails <mongambaclauvet@gmail.com>
2022-07-27 10:31:21 +0100

    10

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
             <nav>
               <input type="text" placeholder="Search" title="Type in a category" id="search">
               <ul>
-                  <li><a href="index.php" target="_blank">Dashboard</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
-                  <li><a href="manageTickets.php" target="_blank">Manage Ticket</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
-                  <li><a href="normalTickets.php" target="_blank">Normal Ticket</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
+                  <li><a href="index.php">Dashboard</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
+                  <li><a href="manageTickets.php">Manage Ticket</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
+                  <li><a href="normalTickets.php">Normal Ticket</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
                   <li><a href="foreignerTickets.php">Foreigner Ticket</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
                   <li><a href="Report.php">Reports</a><img src="CSS/Images/arrow.png" class="nav-arrow"></li>
               </ul>
